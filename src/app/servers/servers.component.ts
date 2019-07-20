3531d9e953a3e1fd03ad3292eadf064626ab4a34
Akshit001 <akshitguptasmart@gmail.com>
2019-07-20 17:42:07 +0530

    Added ngIf else ng-template attribute directive backgroundColor ngClass

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,11 +8,22 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   serverID= 10;
   serverStatus ='offline';
-  
+  serveUp = false;
+  color='red';
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online':'offline';
+    if( this.serverStatus == 'online'){
+        this.serveUp = true;
+    }
+   }
+
+   getColor(){
+    return this.color= this.serverStatus == 'online'? 'green':'red';
+  }
   getServerStatus(){
     return this.serverStatus;
   }
-  constructor() { }
+
 
   ngOnInit() {
   }
